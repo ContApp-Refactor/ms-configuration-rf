6c8d5fd864ade10458248105b43bdd83b1901259
santiagonieto09 <santiago.nieto09@gmail.com>
2025-11-28 18:09:37 -0500

    docs: classesOfDocuments dataAccess

--- src/main/java/co/unicauca/edu/co/contables/configuration/classesOfDocuments/dataAccess/mapper/DocumentClassDataMapper.java
+++ src/main/java/co/unicauca/edu/co/contables/configuration/classesOfDocuments/dataAccess/mapper/DocumentClassDataMapper.java
@@ -7,6 +7,13 @@ import org.mapstruct.Mappings;
 import co.unicauca.edu.co.contables.configuration.classesOfDocuments.dataAccess.entity.DocumentClassEntity;
 import co.unicauca.edu.co.contables.configuration.classesOfDocuments.domain.models.DocumentClass;
 
+/**
+ * @brief Mapeador de datos para clases de documento
+ *
+ * Interfaz que utiliza MapStruct para convertir entre entidades de base de datos
+ * (DocumentClassEntity) y modelos de dominio (DocumentClass).
+ * Gestiona la transformación automática de datos entre capas.
+ */
 @Mapper(componentModel = "spring")
 public interface DocumentClassDataMapper {
     @Mappings({
